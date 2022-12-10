d5d618d3ebca00baf83320bffeae7f167c193d81
Raghav Bang <raghavbang2012@gmail.com>
2022-12-10 14:12:19 +0000

    comment logic

--- index.js
+++ index.js
@@ -1,28 +1,69 @@
 const express = require('express');
+// to read json body we need a middleware for post method
+const bodyParser=require('body-parser');
+
 
 const app = express();
+// Our app will use that middleware
+app.use(bodyParser.json());
+// This convert body data to json
+
+
 
 const productData = {
- products: [
+  products: [
     {
-      id: 1,
+      id: "1",
       name: "Clothes"
     },
     {
-      id: 2,
+      id: "2",
       name: "Jeans"
     }
   ]
 }
 
+
+// HOME page GET method
 app.get('/', (req, res) => {
   res.send('Hello Product Home Page')
 });
 
-app.get('/products', (req, res) => {
 
-  const dataToSend = productData.products.map(item => `<li>${item.name}</li>`).join('\n');
-  res.send(dataToSend);
+// GET method to get all data
+app.get('/products', (req, res) => {
+  res.json(productData.products);
+})
+
+// GET method to get specfic data with help of ID
+
+app.get('/products/:id', (req, res) => {
+
+  const { id } = req.params; // destructing the req here. 
+  // :id in route says that in req.params we have id as key value pair from 
+  //client
+  // so here we destructed it to take id from req.params
+  const product = productData.products.find(product => product.id === id);
+  product ? res.json({ product }) : res.status(404).json({ message: "no product" });
+
+})
+
+// This endpoint will add data to backend. Post method.
+app.post('/products',(req,res)=>{
+  const {products}= req.body; //req.body contains the data that user sent
+  console.log(products);
+  res.status(201).json({echo:"success",product});
+})
+
+// POST method to update the product
+
+app.post('/products',(req,res)=>{
+  const {id}=req.params;
+  const {products}= req.body; //req.body contains the data that user sent
+  const currentProduct = productData.products.find(product => product.id === id);
+  products={...currentProduct,...products}
+  console.log(products);
+  res.status(201).json({echo:"success",product});
 })
 
 
